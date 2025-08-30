9776fe461089b389ae88b307d469920818f32a8e
Sizan Mahmud <sizanmahmud369@gmail.com>
2025-08-30 09:26:54 +0000

    গুগল সাইন ইন এ ক্লিক করার পর ইমেইল সিলেক্ট করে কন্টিনিউতে ক্লিক করার পর

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDlDx1lFR_B5M2mq_sLTZCfjrDLxY5pInk",
   authDomain: "pabnamart.firebaseapp.com",
   projectId: "pabnamart",
-  storageBucket: "pabnamart.firebasestorage.app",
+  storageBucket: "pabnamart.appspot.com",
   messagingSenderId: "600614180848",
   appId: "1:600614180848:web:6f4e21fb4f5b6cd42a6f35",
   measurementId: "YOUR_MEASUREMENT_ID" // Optional: Add your measurement ID
