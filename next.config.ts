64a99951ebad175e447920ea0ed7c1fadf955989
Sizan Mahmud <sizanmahmud369@gmail.com>
2025-09-08 05:19:16 +0000

    সমস্যাটি এখনো রয়ে গেছে তুমি কি এটা ঠিক করতে পারবে না?

---
+++ next.config.ts
@@ -0,0 +1,46 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'share.google',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pix1.wapkizfile.info',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
