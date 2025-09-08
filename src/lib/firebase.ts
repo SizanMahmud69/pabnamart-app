62a26ae34adc0a433fd4f9432c9b8995b5bbc390
Sizan Mahmud <sizanmahmud369@gmail.com>
2025-09-08 02:55:20 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,24 +5,17 @@ import { initializeApp, getApp, getApps, type FirebaseApp, type FirebaseOptions 
 import { getMessaging, type Messaging } from 'firebase/messaging';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "pabnamart",
+  "appId": "1:600614180848:web:6f4e21fb4f5b6cd42a6f35",
+  "storageBucket": "pabnamart.appspot.com",
+  "apiKey": "AIzaSyDlDx1lFR_B5M2mq_sLTZCfjrDLxY5pInk",
+  "authDomain": "pabnamart.firebaseapp.com",
+  "messagingSenderId": "600614180848"
 };
 
 function createFirebaseApp(config: FirebaseOptions): FirebaseApp {
     if (getApps().length > 0) {
         return getApp();
-    }
-
-    if (!config.apiKey || !config.projectId) {
-        console.error("Firebase config is missing API key or Project ID.");
-        // Return a dummy app object or handle this case as needed
-        // This helps prevent crashes during server-side rendering or build.
-        return {} as FirebaseApp;
     }
 
     return initializeApp(config);
