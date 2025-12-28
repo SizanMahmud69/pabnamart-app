8b75b1a8406e400a58d7300162de6fb1bf8982ba
Sizan Mahmud <sizanmahmud369@gmail.com>
2025-12-28 14:47:55 +0000

    Error: Failed to upload image.     at handleSubmit (https://6000-firebas

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,11 +2,9 @@ import { handleUpload, type HandleUploadBody } from '@vercel/blob/client';
 import { NextResponse } from 'next/server';
  
 export async function POST(request: Request): Promise<NextResponse> {
-  const body = (await request.json()) as HandleUploadBody;
- 
   try {
     const jsonResponse = await handleUpload({
-      body,
+      body: request.body,
       request,
       onBeforeUpload: async (pathname) => {
         // Generate a random pathname for the blob
