d3216375cd0394f6d6293f19d397128dad166053
Sizan Mahmud <sizanmahmud369@gmail.com>
2025-12-29 12:36:49 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- vercel/path0/next.config.ts
+++ vercel/path0/next.config.ts
@@ -1,6 +1,5 @@
 
 import type {NextConfig} from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -45,16 +44,6 @@ const nextConfig: NextConfig = {
         hostname: '**.public.blob.vercel-storage.com',
       }
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const swDest = path.join(__dirname, 'public', 'firebase-messaging-sw.js');
-      config.resolve.alias['firebase/messaging'] = path.resolve(
-        __dirname,
-        'node_modules/firebase/messaging'
-      );
-    }
-    return config;
   },
 };
 
